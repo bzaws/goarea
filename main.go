21aef92e506aa9f3e112adc404e450917464bc39
Bruno Martins de Oliveira Gonçalves <93011050+bzaws@users.noreply.github.com>
2022-03-14 12:00:50 -0300

    Update main.go

--- main.go
+++ main.go
@@ -1,18 +1,21 @@
 package goarea
+
 import "math"
+
 //Pi é uma proporção numérica definida pela relação dentr
 // o périmetro de uma circunferência e seu diâmetro
 const Pi = 3.1416
+
 //Circ é responsável por calcular a área da circunferência
-func Circ(raio float64) {
+func Circ(raio float64) float64 {
   return math.Pow(raio, 2) * Pi
 }
 
-//React é responsável por calcular a área de um retangulo
-func React(base, altura float64) float64 {
+//Rect é responsável por calcular a área de um retangulo
+func Rect(base, altura float64) float64 {
   return base * altura
 }
 // Não é visível!
-func _TrianguloEoq(base, altura float64) float64 {
+func _TrianguloEq(base, altura float64) float64 {
   return (base * altura) / 2
 }
